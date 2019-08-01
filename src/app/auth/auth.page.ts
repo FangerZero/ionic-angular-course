4aca46e6d047dd4ca60d07cd6a5759a30a23811a
Kelly Plageman <KPlageman@prokarma.com>
2019-08-01 10:47:40 -0500

    What I have so far, does not work correctly right now

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -29,7 +30,10 @@ export class AuthPage implements OnInit {
         }, 1500);
       });
     // Redirect happening inside timeout to simulate "Loading Time"
-   
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
   }
 
 }
